5253ff49e1f72e50723552e5927a8aff4fe4671e
Roger Leblanc <roger@rogerleblanc.net>
2017-03-09 01:26:10 -0500

    Initial release
    Initial release

---
+++ Additions/Additions/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Additions
+{
+    public partial class MainPage : ContentPage
+    {
+        public static readonly BindableProperty TotalValueProperty = BindableProperty.Create(nameof(TotalValue), typeof(int), typeof(MainPage), 0);
+
+        public int TotalValue
+        {
+            get { return (int)GetValue(TotalValueProperty); }
+            set { SetValue(TotalValueProperty, value); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            TotalValue = FirstNumber.NumberValue + SecondNumber.NumberValue;
+
+            BindingContext = this;
+        }
+
+        private void OnNumberValueChanged(object sender, EventArgs e)
+        {
+            TotalValue = FirstNumber.NumberValue + SecondNumber.NumberValue;
+        }
+    }
+}
---
+++ Additions/Additions/NumberView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Additions
+{
+    public delegate void NumberValueChangedEventHandler(object sender, EventArgs e);
+
+    public partial class NumberView : StackLayout
+    {
+        public event EventHandler NumberValueChanged;
+
+        public static readonly BindableProperty NumberValueProperty = BindableProperty.Create(nameof(NumberValue), typeof(int), typeof(NumberView), 1);
+
+        public int NumberValue
+        {
+            get { return (int)GetValue(NumberValueProperty); }
+            set { SetValue(NumberValueProperty, value); }
+        }
+
+        public NumberView()
+        {
+            InitializeComponent();
+
+            NumberValue = 1;
+            BindingContext = this;
+        }
+
+        private void Up_Button_Clicked(object sender, EventArgs e)
+        {
+            NumberValue++;
+
+            OnNumberValueChanged(EventArgs.Empty);
+        }
+
+        private void Down_Button_Clicked(object sender, EventArgs e)
+        {
+            NumberValue--;
+            if (NumberValue <= 0)
+                NumberValue = 1;
+
+            OnNumberValueChanged(EventArgs.Empty);
+        }
+
+        protected virtual void OnNumberValueChanged(EventArgs e)
+        {
+            if (NumberValueChanged != null)
+                NumberValueChanged(this, e);
+        }
+    }
+}
